6d3babdf1b68bd6d78ef618b5e78e0ac1078108b
Lukas <lukas.heinrich@gmail.com>
2021-06-17 14:57:41 +0200

    Simple array roundtrip (#9)
    * make python test work
    
    * simple array roundtrip
    
    * simple struct build
    
    Co-authored-by: Giordon Stark <kratsg@gmail.com>

--- src/babel/python.cpp
+++ src/babel/python.cpp
@@ -6,6 +6,8 @@
 
 #include <arrow/python/pyarrow.h>
 #include <arrow/array/builder_primitive.h>
+#include <arrow/array.h>
+#include <arrow/compute/function.h>
 
 namespace py = pybind11;
 using DoubleArray = arrow::DoubleArray;
@@ -33,9 +35,71 @@ double sum(py::handle& source) {
     return sum;
 }
 
+py::handle times_two(py::handle& source) {
+    PyObject* src = source.ptr();
+
+    if(!arrow::py::is_array(src)){
+        std::cout << "not an array" << std::endl;
+    }
+
+    arrow::Result<std::shared_ptr<arrow::Array>> result = arrow::py::unwrap_array(src);
+    if(!result.ok()){
+        std::cout << "not ok" << std::endl;
+
+    }
+
+    std::shared_ptr<arrow::DoubleArray> a = std::static_pointer_cast<arrow::DoubleArray>(result.ValueOrDie());
+    std::cout << "a: " << a << std::endl;
+    std::cout << "a: " << a->values()->data() << std::endl;
+    std::cout << "a: " << a->ToString() << std::endl;
+
+
+    arrow::DoubleBuilder builder;
+    builder.Resize(3);
+
+    std::vector<bool> validity = {true, true, true};
+    std::vector<double> values = {3.1, 4.2 ,5.3};
+
+    builder.AppendValues(values, validity);
+    
+    std::shared_ptr<arrow::Array> array;
+    arrow::Status st = builder.Finish(&array);
+
+    if(!st.ok()){
+        std::cout << "not ok" << std::endl;
+    }
+
+    return arrow::py::wrap_array(array);
+}
+
+std::shared_ptr<arrow::Array> build(const std::vector<double>& values, const std::vector<bool>& validity){
+    arrow::DoubleBuilder builder;
+    builder.Resize(3);
+    builder.AppendValues(values, validity);
+    std::shared_ptr<arrow::Array> array;
+    arrow::Status st = builder.Finish(&array);
+    if(!st.ok()){
+        std::cout << "not ok" << std::endl;
+    }
+    return array;
+}
+
+py::handle handle_struct() {
+    auto pt = build({3.1, 4.2 ,5.3},{true, true, true});
+    auto eta = build({0.1, 0.2 ,0.3},{true, true, true});
+
+    std::vector<std::string> fields = {"pt","eta"};
+    auto x = arrow::StructArray::Make(
+        {pt,eta}, fields
+    ).ValueOrDie();
+    return arrow::py::wrap_array(x);
+}
+
 PYBIND11_MODULE(babel, m) {
   // Ensure dependencies are loaded.
   arrow::py::import_pyarrow();
 
   m.def("sum", &sum, py::call_guard<py::gil_scoped_release>());
+  m.def("times_two", &times_two);
+  m.def("handle_struct", &handle_struct);
 }
--- test-python.py
+++ test-python.py
@@ -14,6 +14,7 @@ def test_producer():
 
 import pyarrow as pa
 import numpy as np
+import awkward as ak
 
 import build.babel
 
@@ -23,6 +24,19 @@ def test_sum():
     assert build.babel.sum(x) == np.sum(np.array(data))
     assert build.babel.sum(x[1:]) == np.sum(np.array(x[1:]))
 
+def test_awkward_roundtrip():
+    in_arr = ak.Array([1.,2.,3.])
+    in_arrow = ak.to_arrow(in_arr)
+    out_arrow = build.babel.times_two(in_arrow)
+    out_arr = ak.from_arrow(out_arrow)
+    assert ak.to_numpy(out_arr).tolist() ==  [3.1, 4.2, 5.3]  
+
+def test_struct():
+    s = build.babel.handle_struct()
+    a = ak.from_arrow(s)
+    assert str(ak.type(a)) == '3 * {"pt": ?float64, "eta": ?float64}'
 
 if __name__ == '__main__':
     test_sum()
+    test_awkward_roundtrip()
+    test_struct()
